f05a8927aa76790c3bec5a866f91761914e9cafa
marioesperalta <marioesperaltadelgado@gmail.com>
2018-06-08 00:14:31 +0200

    Modificación controllers/tip

--- controllers/tip.js
+++ controllers/tip.js
@@ -6,41 +6,44 @@ const {models} = require("../models");
 exports.load = (req, res, next, tipId) => {
 
     models.tip.findById(tipId)
-    .then(tip => {
-        if (tip) {
-            req.tip = tip;
-            next();
-        } else {
-            next(new Error('There is no tip with tipId=' + tipId));
-        }
-    })
-    .catch(error => next(error));
+        .then(tip => {
+            if (tip) {
+                req.tip = tip;
+                next();
+            } else {
+                next(new Error('There is no tip with tipId=' + tipId));
+            }
+        })
+        .catch(error => next(error));
 };
 
 
 // POST /quizzes/:quizId/tips
 exports.create = (req, res, next) => {
- 
+
+    const authorId = req.session.user && req.session.user.id || 0;
+
     const tip = models.tip.build(
         {
             text: req.body.text,
-            quizId: req.quiz.id
+            quizId: req.quiz.id,
+            authorId
         });
 
-    tip.save()
-    .then(tip => {
-        req.flash('success', 'Tip created successfully.');
-        res.redirect("back");
-    })
-    .catch(Sequelize.ValidationError, error => {
-        req.flash('error', 'There are errors in the form:');
-        error.errors.forEach(({message}) => req.flash('error', message));
-        res.redirect("back");
-    })
-    .catch(error => {
-        req.flash('error', 'Error creating the new tip: ' + error.message);
-        next(error);
-    });
+    tip.save({fields:["text","quizId","authorId"]})
+        .then(tip => {
+            req.flash('success', 'Tip created successfully.');
+            res.redirect("back");
+        })
+        .catch(Sequelize.ValidationError, error => {
+            req.flash('error', 'There are errors in the form:');
+            error.errors.forEach(({message}) => req.flash('error', message));
+            res.redirect("back");
+        })
+        .catch(error => {
+            req.flash('error', 'Error creating the new tip: ' + error.message);
+            next(error);
+        });
 };
 
 
@@ -52,14 +55,14 @@ exports.accept = (req, res, next) => {
     tip.accepted = true;
 
     tip.save(["accepted"])
-    .then(tip => {
-        req.flash('success', 'Tip accepted successfully.');
-        res.redirect('/quizzes/' + req.params.quizId);
-    })
-    .catch(error => {
-        req.flash('error', 'Error accepting the tip: ' + error.message);
-        next(error);
-    });
+        .then(tip => {
+            req.flash('success', 'Tip accepted successfully.');
+            res.redirect('/quizzes/' + req.params.quizId);
+        })
+        .catch(error => {
+            req.flash('error', 'Error accepting the tip: ' + error.message);
+            next(error);
+        });
 };
 
 
@@ -67,10 +70,18 @@ exports.accept = (req, res, next) => {
 exports.destroy = (req, res, next) => {
 
     req.tip.destroy()
-    .then(() => {
-        req.flash('success', 'tip deleted successfully.');
-        res.redirect('/quizzes/' + req.params.quizId);
-    })
-    .catch(error => next(error));
+        .then(() => {
+            req.flash('success', 'tip deleted successfully.');
+            res.redirect('/quizzes/' + req.params.quizId);
+        })
+        .catch(error => next(error));
 };
 
+exports.new= (req,res,next)=>{
+
+    const tip = {
+        text:""
+    };
+    const{quiz}=req;
+    res.render('tips/new', {tip,quiz});
+}
